b3f58c24875b2b5e46bb85f766d0ca7d6967cffb
JLTrincado <juanluis.trincado@upf.edu>
2021-01-01 18:19:41 +0100

    Adapting Neoskipping_part2.py

--- Neoskipping_ISOTOPE_part2.py
+++ Neoskipping_ISOTOPE_part2.py
@@ -10,6 +10,9 @@ from lib.Neoskipping.run_netMHC_classI_slurm_part2 import *
 from lib.Neoskipping.run_netMHCpan_classI_slurm_part2 import *
 from lib.Neoskipping.format_to_SPADA import *
 
+from argparse import ArgumentParser, RawTextHelpFormatter
+import argparse
+
 # create logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -28,17 +31,40 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
-def main():
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+description = \
+"Description: Get alternative splice site events\n\n"
+
+parser = ArgumentParser(description=description, formatter_class=RawTextHelpFormatter,
+                        add_help=True)
+parser.add_argument("-HLAclass", "--HLAclass", required=True, help = "HLA genotype of the samples")
+parser.add_argument("-HLAtypes", "--HLAtypes", required=True, help = "HLA alelles recognized by NetMHC")
+parser.add_argument("-HLAtypespan", "--HLAtypespan", required=True, help = "HLA alelles recognized by NetMHCpan")
+parser.add_argument("-netMHC", "--netMHC", required=True, help = "netMHC path")
+parser.add_argument("-netMHCpan", "--netMHCpan", required=True, help = "netMHCpan path")
+parser.add_argument("-o", "--output", required=True, help = "Output path")
+
+# def main():
+def main(HLAclass_path, HLAtypes_path, HLAtypes_pan_path, netMHC_path, netMHC_pan_path, output_path):
     try:
 
         logger.info("Starting execution")
 
-        HLAclass_path = "/projects_rg/SCLC_cohorts/Smart/PHLAT/PHLAT_summary_ClassI.out"
-        HLAtypes_path = "/projects_rg/SCLC_cohorts/tables/NetMHC-4.0_HLA_types_accepted.tab"
-        HLAtypes_pan_path = "/projects_rg/SCLC_cohorts/tables/NetMHCpan-4.0_HLA_types_accepted.tab"
-        netMHC_path = "/projects_rg/SCLC_cohorts/soft/netMHC-4.0/netMHC"
-        netMHC_pan_path = "/projects_rg/SCLC_cohorts/soft/netMHCpan-4.0/netMHCpan"
-        output_path = "/users/genomics/juanluis/SCLC_cohorts/Smart/epydoor/neoskipping"
+        # HLAclass_path = "/projects_rg/SCLC_cohorts/Smart/PHLAT/PHLAT_summary_ClassI.out"
+        # HLAtypes_path = "/projects_rg/SCLC_cohorts/tables/NetMHC-4.0_HLA_types_accepted.tab"
+        # HLAtypes_pan_path = "/projects_rg/SCLC_cohorts/tables/NetMHCpan-4.0_HLA_types_accepted.tab"
+        # netMHC_path = "/projects_rg/SCLC_cohorts/soft/netMHC-4.0/netMHC"
+        # netMHC_pan_path = "/projects_rg/SCLC_cohorts/soft/netMHCpan-4.0/netMHCpan"
+        # output_path = "/users/genomics/juanluis/SCLC_cohorts/Smart/epydoor/neoskipping"
 
         # 10. Run netMHC-4.0_part2
         logger.info("Part10...")
@@ -56,12 +82,12 @@ def main():
                                       output_path + "/neoskipping_NetMHCpan-4.0_neoantigens_type_2_all.tab", output_path + "/neoskipping_NetMHCpan-4.0_junctions_ORF_neoantigens.tab",
                                       netMHC_pan_path)
 
-        # 12. Run format_to_SPADA
-        logger.info("Part12...")
-        format_to_SPADA(output_path + "/all_neoskipping_ORF.tab", output_path + "/all_neoskipping_ORF_sequences.tab",
-                        output_path + "/all_neoskipping_Interpro.tab",
-                        output_path + "/all_neoskipping_IUPred.tab", output_path + "/all_neoskipping_SPADA.tab",
-                        output_path + "/all_neoskipping_SPADA.fasta", output_path + "/all_neoskipping_SPADA_features.tab")
+        # # 12. Run format_to_SPADA
+        # logger.info("Part12...")
+        # format_to_SPADA(output_path + "/all_neoskipping_ORF.tab", output_path + "/all_neoskipping_ORF_sequences.tab",
+        #                 output_path + "/all_neoskipping_Interpro.tab",
+        #                 output_path + "/all_neoskipping_IUPred.tab", output_path + "/all_neoskipping_SPADA.tab",
+        #                 output_path + "/all_neoskipping_SPADA.fasta", output_path + "/all_neoskipping_SPADA_features.tab")
         logger.info("Done.")
 
         exit(0)
