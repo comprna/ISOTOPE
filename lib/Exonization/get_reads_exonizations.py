0c505c35da6f7ff934ab35f9c29e63a4eea596cc
JLTrincado <juanluis.trincado@upf.edu>
2021-03-18 11:52:07 +0100

    Testing exonizations pipeline (convert totalCounts into list for zip)

--- lib/Exonization/get_reads_exonizations.py
+++ lib/Exonization/get_reads_exonizations.py
@@ -155,13 +155,14 @@ def get_reads_exonizations(exonization_file, readCounts_file, output_file, Intro
                 totalCounts1 = list(totalCounts1)
             if not isinstance(totalCounts2, list):
                 totalCounts2 = list(totalCounts2)
+            logger.info("Processing files6...")
             logger.info(totalCounts1)
             logger.info(totalCounts2)
-            logger.info("Processing files6...")
-            logger.info(list(zip(totalCounts1, totalCounts2)))
             logger.info("Processing files7...")
-            final_counts = [str(min(x,y)) for x, y in zip(totalCounts1, totalCounts2)]
+            logger.info(list(zip(totalCounts1, totalCounts2)))
             logger.info("Processing files8...")
+            final_counts = [str(min(x,y)) for x, y in zip(totalCounts1, totalCounts2)]
+            logger.info("Processing files9...")
             aux = "\t".join(final_counts)
             final_counts_list.append(aux)
 
